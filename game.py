346f8cc26c4748f18c7a102583bad9734aa4ac22
Anabel Villalobos <88516795+annvilla1998@users.noreply.github.com>
2022-12-04 20:21:29 -0800

    added rooms dictionary

--- game.py
+++ game.py
@@ -2,5 +2,12 @@
 
 def game():
     rooms = {
-        'Living Room': { ''}
+        'Living Room': {'North': 'Bedroom', 'West': 'Backyard', 'East': 'Laundry Room', 'South': 'Dining Room'},
+        'Backyard': {'East': 'Living Room', 'item': 'stake'},
+        'Bedroom': {'East': 'Bathroom', 'South': 'Living Room', 'item': 'apple'},
+        'Bathroom': {'West': 'Bedroom', 'item': 'Vampire'},
+        'Dining Room': {'North': 'Living Room', 'East': 'Kitchen', 'item': 'mirror'},
+        'Kitchen': {'West': 'Dining Room', 'item': 'garlic'},
+        'Laundry Room': {'West': 'Living Room', 'North': 'Garage', 'item': 'rosemary'},
+        'Garage': {'South': 'Laundry Room', 'item': 'sunblade'}
     }
