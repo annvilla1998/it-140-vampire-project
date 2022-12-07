563772179dc0dff48b94568e53f44dcd7aceb688
Anabel Villalobos <88516795+annvilla1998@users.noreply.github.com>
2022-12-07 13:26:20 -0800

    finished game

--- TextBasedGame.py
+++ TextBasedGame.py
@@ -25,45 +25,74 @@ def playGame():
     # define user inventory as an empty list
     inventory = []
 
-    # WHILE True:
     while True:
-        print("You are in the {}".format(current_room))
-        print("Inventory: {}".format(inventory))
-        if 'item' in current_room:
-            print("You see a {}".format(current_room.item))
-        print("----------------------------------")
-        print("Enter your move")
-    # 	IF the inventory length is equal to 6
-    # 		RETURN “You have defeated the vampire!”
-    # 	ELIF the currentRoom is equal to the “Bathroom”
-    # 		RETURN “You see the vampire GAME OVER”
-    # 	ELSE
-    # 		run function for user input
-    #
-    # 	get user input for their move
-    # 	split the array
-    #
-    # 	IF the first element in the array is not get or go
-    # 		THEN print an error message
+        # if the player entered exit
+        if current_room.lower() == 'exit':
+            # exit the game
+            print("Thanks for playing the game. Hope you enjoyed it.")
+            break
+        # if the player picked up the last item
+        if len(inventory) == 6:
+            # exit the game
+            print("Congratulations! You have collected all items and defeated the vampire!")
+            print("Thanks for playing the game. Hope you enjoyed it.")
+            break
+        # if the player enters the room with the vampire
+        elif current_room == "Bathroom":
+            # exit the game
+            print("HISSSS GAME OVER")
+            print("Thanks for playing the game. Hope you enjoyed it.")
+            break
+        else:
+        # continue with the game
+            print("You are in the {}".format(current_room))
+            print("Inventory: {}".format(inventory))
+            if 'item' in rooms[current_room]:
+                print("You see a {}".format(rooms[current_room]['item']))
+            print("----------------------------------")
+            print("Enter your move")
+
+            #otherwise get new user input
+            player_move = input()
+
+            # 	split into array
+            move_arr = player_move.split(" ")
+
+            if move_arr[0] == "go":
+                direction = move_arr[1]
+                # if the direction is in the current room dictionary
+                if direction in rooms[current_room]:
+                    # set the current room equal to that room
+                    current_room = rooms[current_room][direction]
+                else:
+                    print("You can't go that way")
+            elif move_arr[0] == "get":
+                current_item = move_arr[1]
+                # if the item is in the current room
+                if rooms[current_room]['item'] == current_item:
+                    # add the item to the inventory
+                    # add this value into the inventory array
+                    inventory.append(current_item)
+                    # delete the item key in the rooms.current_room dictionary
+                    del rooms[current_room]['item']
+                    print(rooms[current_room])
+                else:
+                    print("Can't get {}!".format(current_item))
 
 
-    # Pseudocode or Flowchart for Code to “Move Between Rooms”
-    #
-    # 	IF the first value in the array is “go”
-    # 		IF the second value is in rooms.current_room
-    # 			THEN set the current_room to equal rooms.current_room
-    # 		ELSE
-    # 			THEN PRINT “You can’t go that way”
-    #
-    # Pseudocode or Flowchart for Code to “Get an Item”
-    #
-    # 	IF the first element in the array is “get”
-    # 		IF the second value is rooms.current_room.item
-    # 			add this value into the inventory array
-    # 			delete the item key in the rooms.current_room dictionary
-    # 			PRINT “{Item} retrieved!”
-    # 		ELSE
-    # 			PRINT an error message
+            # if the player input is exit
+            elif move_arr[0].lower() == "exit":
+                # then the current room is set to exit
+                current_room = "exit"
+            else:
+                # handle input validation error
+                print("Invalid command")
+
+
+
+
+
+
 
 
 if __name__ == '__main__':
